44a71e38bb42d71fc96e05c175f32fc75a664de1
Chloe Christine Fontenot <foley2431@gmail.com>
2026-05-28 16:58:35 -0500

    fix tray manager for Linux

--- packages/tray_manager/linux/tray_manager_plugin.cc
+++ packages/tray_manager/linux/tray_manager_plugin.cc
@@ -102,6 +102,7 @@ static FlMethodResponse* destroy(TrayManagerPlugin* self, FlValue* args) {
   if (indicator) {
     app_indicator_set_status(indicator, APP_INDICATOR_STATUS_PASSIVE);
     g_object_unref(indicator);
+    indicator = nullptr;
   }
   return FL_METHOD_RESPONSE(
       fl_method_success_response_new(fl_value_new_bool(true)));
