16a0ad534b44325302897e3fadb367208492a639
Chloe Christine Fontenot <foley2431@gmail.com>
2026-05-28 16:39:18 -0500

    fix tray manager for Linux

--- packages/tray_manager/linux/tray_manager_plugin.cc
+++ packages/tray_manager/linux/tray_manager_plugin.cc
@@ -100,7 +100,7 @@ GtkWidget* _create_menu(FlValue* args) {
 
 static FlMethodResponse* destroy(TrayManagerPlugin* self, FlValue* args) {
   if (!(!indicator)) {
-    app_indicator_set_status(indicator, APP_INDICATOR_STATUS_PASSIVE);
+    indicator.g_object_ref = 0;
   }
   return FL_METHOD_RESPONSE(
       fl_method_success_response_new(fl_value_new_bool(true)));
