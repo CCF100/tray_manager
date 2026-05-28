f01f86e8d9dcb677d7816b35268a208f69485cab
Chloe Christine Fontenot <foley2431@gmail.com>
2026-05-28 17:11:18 -0500

    fix tray manager for Linux

--- packages/tray_manager/linux/tray_manager_plugin.cc
+++ packages/tray_manager/linux/tray_manager_plugin.cc
@@ -120,10 +120,8 @@ static FlMethodResponse* set_icon(TrayManagerPlugin* self, FlValue* args) {
     menu = gtk_menu_new();
 
   if (!indicator) {
-    indicator = app_indicator_new(
-      id,
-      icon_path,
-      APP_INDICATOR_CATEGORY_APPLICATION_STATUS);
+    app_indicator_set_status(indicator,
+                             APP_INDICATOR_STATUS_PASSIVE);
 
     app_indicator_set_menu(indicator, GTK_MENU(menu));
     gtk_widget_show_all(menu);
